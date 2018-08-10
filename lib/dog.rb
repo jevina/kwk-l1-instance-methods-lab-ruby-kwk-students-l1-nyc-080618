42c350c59351306acffa03dbae225133e7759834
jevina <jwong00@stuy.edu>
2018-08-10 15:16:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# dog.rb
+# # dog.rb
+
+# class Dog
+#   def initialize(breed, gender, sexual_preference)
+#     @breed = breed
+#     @gender = gender
+#     @sexual_preference = sexual_preference
+#   end
+  
+#   def breed
+#     @breed
+#   end
+  
+#   def gender
+#     @gender
+#   end
+  
+#   def sexual_preference
+#     @sexual_preference
+#   end
+  
+#   def gender= (new_gender)
+#   @gender=new_gender
+#   end
+# end
+
+class Dog
+end
+
+breed = Dog.new
+gender = Dog.new
+sexual_preference = Dog.new
